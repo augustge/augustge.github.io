0628dd422970fff5e671a3e710b26d0e65530a0c
August Geelmuyden <augustgeelmuyden92@gmail.com>
2018-08-29 22:35:04 +0200

    posts

---
+++ mainpagescript.js
@@ -0,0 +1,17 @@
+
+var canvas;
+
+
+function setup(){
+  canvas = createCanvas(windowWidth,windowHeight);
+
+  div = createDiv("test")
+  div.position(width/2,height/2)
+  div.attribute("w3-include-html","Brachistochroneproblemet.html")
+  
+
+}
+
+function draw(){
+  // background(0);
+}
