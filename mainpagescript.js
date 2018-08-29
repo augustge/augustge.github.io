61da51ff921baa6977a737e16c1186a3ba6c02cb
August Geelmuyden <augustgeelmuyden92@gmail.com>
2018-08-29 22:47:13 +0200

    posts

--- mainpagescript.js
+++ mainpagescript.js
@@ -1,19 +1,39 @@
 
 var canvas;
 
+var div;
 
 function setup(){
   canvas = createCanvas(windowWidth,windowHeight);
 
-  div = createDiv("test")
+  div = createDiv("BOBOBO TATATA")
   div.position(width/2,height/2)
   div.attribute("w3-include-html","Brachistochroneproblemet.html")
 
-  var text = get_file("Brachistochroneproblemet.html",True);
-  div.html(text)
+
 
 }
 
 function draw(){
   // background(0);
+}
+
+function mousePressed(){
+  var text = get_file("Brachistochroneproblemet.html",True);
+  div.html(text)
+}
+
+
+function get_file(url, callback)
+{
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", url, true);
+    xmlhttp.onreadystatechange = function()
+    {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
+        {
+            callback(xmlhttp.responseText);
+        }
+    }
+    xmlhttp.send();
 }
