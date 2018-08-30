65c717a1c3058de8f3095b7156ded46e2e1c5e98
August Geelmuyden <augustgeelmuyden92@gmail.com>
2018-08-30 18:29:14 +0200

    testing

--- mainpagescript.js
+++ mainpagescript.js
@@ -4,15 +4,19 @@ var canvas;
 var div;
 var POSTS = [];
 
-var texter;
+var pagesElm, postsElm;
 
 function preload(){
-  texter = document.getElementById('1');
+  pagesElm = document.getElementById('Pages');
+  postsElm = document.getElementById('Posts');
 }
 
 function setup(){
   canvas = createCanvas(windowWidth,windowHeight);
   POSTS.push( new Post("Brachistochroneproblemet.html","HOOPLA"))//"Brachistochroneproblemet") )
+  POSTS.push( new Post("Brachistochroneproblemet.html","HIIPLA"))
+  POSTS.push( new Post("Brachistochroneproblemet.html","HOOKLA"))
+  POSTS.push( new Post("Brachistochroneproblemet.html","HOOFLA"))
 
 
 }
@@ -24,25 +28,28 @@ function draw(){
 function Post(filename,title){
   this.filename = filename
   this.title = title
-  this.header = createElement("h3","KOKO")
-  this.header.id("design")
-  this.header.attribute("title","BOBO")
-  this.header.parent(texter);
+  this.header = createElement("h3","FAFA")
+  // this.header.id("design")
+  // this.header.attribute("title","BOBO")
+  this.header.parent(postsElm);
+  // <h3 id="design" class="select" title="Posts">Select a Design:</h3>
   this.DIV = createDiv(title);
   this.DIV.position(width/2,height/2);
+  console.log("this is 'this.DIV':")
+  console.log(this.DIV)
+  console.log("--> end")
 
   this.load = function(){
     console.log("TRYING")
     var infile = loadStrings("Brachistochroneproblemet.txt",this.loadCallback);
+    console.log("IN LOAD:")
+    console.log(infile)
     this.showPost();
   }
 
   this.loadCallback = function(txt){
     this.TEXT = txt
     text(txt,width/2,height/2)
-    console.log("this is 'this.DIV':")
-    console.log(this.DIV)
-    console.log("--> end")
     // console.log(this.DIV)
     // console.log("END PRINT")
     // console.log(txt)
