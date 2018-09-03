fd387c5113b2a92c0a2d55251b87ad87713b26e3
August Geelmuyden <augustgeelmuyden92@gmail.com>
2018-09-03 18:03:01 +0200

    CSS-Tag filtering with AngularJS + GoogleAnalytics

--- my_angularjs_script.js
+++ my_angularjs_script.js
@@ -14,3 +14,189 @@ app.config(function($routeProvider) {
 var viewCtrl =  app.controller("postController", function ($scope,$route) {
   MathJax.Hub.Queue(["Typeset",MathJax.Hub]); // redo mathjax typesetting
 });
+
+
+var mainCtrl = app.controller("mainCtrl", function ($scope) {
+  $scope.languages = {
+    "norwegian": {
+      name: 'Norwegian',
+      checkedDoc:   true,
+      checkedPost:  true
+    },
+    "english": {
+      name: 'English',
+      checkedDoc:   true,
+      checkedPost:  true
+    }
+  };
+  $scope.tags = {
+      "math": {
+      name: 'Mathematics',
+      indicator: "math",
+      checkedAnim:  true,
+      checkedDoc:   true,
+      checkedPost:  true},
+      "phys": {
+      name: 'Physics',
+      indicator: "phys",
+      checkedAnim:  true,
+      checkedDoc:   true,
+      checkedPost:  true},
+      "prog": {
+      name: 'Programming',
+      indicator: "prog",
+      checkedAnim:  true,
+      checkedDoc:   true,
+      checkedPost:  true},
+      "other": {
+      name: 'Other',
+      indicator: "other",
+      checkedAnim:  true,
+      checkedDoc:   true,
+      checkedPost:  true}
+  };
+
+  $scope.postItems = [
+    {
+      name: 'Brachistochroneproblemet',
+      description: "This is a post about the famous Brachistochrone problem which introduced variational calculus. This includes a simple physics question -- what is the trajectory that makes a ball roll fastest from one point to another? We take a look at Newton's ingenious solution to this problem.",
+      reference: "Brachistochroneproblemet",
+      tag: $scope.tags["phys"],
+      language: $scope.languages["norwegian"]},
+    {
+      name: 'Baselproblemet',
+      description: "A text on the solution of the famous 'Basel Problem'.",
+      reference: "Baselproblemet",
+      tag: $scope.tags["math"],
+      language: $scope.languages["norwegian"]},
+    {
+      name: 'Fluid Mechanics',
+      description: "Text on basic equations in fluid dynamics.",
+      reference: "FluidMechanics",
+      tag: $scope.tags["phys"],
+      language: $scope.languages["english"]},
+    {
+      name: 'Test post',
+      description: "This is just a post to test functionality of the web page.",
+      reference: "FirstPost",
+      tag: $scope.tags["prog"],
+      language: $scope.languages["english"]}
+    ];
+
+    $scope.docItems = [
+      {
+        name: 'Linearized gravity',
+        description: "Notes on linear perturbations of Minkowskian spacetime and gravitational waves.",
+        reference: "Linearized gravity.pdf",
+        tag: $scope.tags["phys"],
+        language: $scope.languages["english"]},
+      {
+        name: 'Advanced Quantum Field theory',
+        description: 'A brief and incomplete introduction to quantum field theory.',
+        reference: 'Advanced Quantum Field theory.pdf',
+        tag: $scope.tags["phys"],
+        language: $scope.languages["english"]},
+      {
+        name: 'Atomic modelling of Argon lattice',
+        description: 'A project in Computational physics about simulating thermodynamic properties of a FCC crystal of argon atoms.',
+        reference: 'Atomic modelling of Argon lattice.pdf',
+        tag: $scope.tags["prog"],
+        language: $scope.languages["english"]},
+      {
+        name: 'Classical Mechanics and Minimal Action',
+        description: 'Brief and incomplete notes on Classical Mechanics.',
+        reference: 'Classical Mechanics and Minimal Action.pdf',
+        tag: $scope.tags["phys"],
+        language: $scope.languages["english"]},
+      {
+        name: 'En hyllest til Euklid.pdf',
+        description: "A text on proposisjon 35 in book III of Euclid's Elements with some notes on the modern onderstanding of the statement.",
+        reference: 'En hyllest til Euklid.pdf',
+        tag: $scope.tags["math"],
+        language: $scope.languages["norwegian"]},
+      {
+        name: 'Felttur i elektromagnetisme 2017',
+        description: 'Notes for lectures held on a field trip in an introductory course on Electromagnetism.',
+        reference: 'Felttur i elektromagnetisme 2017.pdf',
+        tag: $scope.tags["phys"],
+        language: $scope.languages["norwegian"]},
+      {
+        name: 'Spinntur',
+        description: 'Notes for lectures held on a field trip about Angular Momentum.',
+        reference: 'Spinntur.pdf',
+        tag: $scope.tags["phys"],
+        language: $scope.languages["norwegian"]},
+      {
+        name: 'The Faddeev-Popov process',
+        description: 'Notes on the so-called Faddeev-Popov process that enables calculation of a non-divergent expression for the photon propagator.',
+        reference: 'The Faddeev-Popov process.pdf',
+        tag: $scope.tags["phys"],
+        language: $scope.languages["english"]},
+      {
+        name: 'The Schwinger-Dyson equations',
+        description: 'Notes on the Schwinger-Dyson equations that serve as a quantum analogue of the equations of motion.',
+        reference: 'The Schwinger-Dyson equations.pdf',
+        tag: $scope.tags["phys"],
+        language: $scope.languages["english"]}
+      ];
+
+
+      $scope.animItems = [
+        {
+          name: 'Black Hole Raytracing',
+          description: 'A very simple raytracing simulation of a black hole attached to the mouse lensing a star in the Andromeda galaxy.',
+          reference: 'Black Hole Raytracing/',
+          tag: $scope.tags["phys"]
+        },{
+          name: 'Chaos fractal generator',
+          description: 'A visualization of how pretty fractals can emerge from probabilistic processes.',
+          reference: 'Chaos fractal generator/',
+          tag: $scope.tags["math"]
+        },{
+          name: 'Double Pendulum',
+          description: 'A simple visualization of a double pendulum.',
+          reference: 'Double Pendulum/',
+          tag: $scope.tags["phys"]
+        },{
+          name: 'Elastic collisions',
+          description: 'Visualization of the physics of elastic collisions.',
+          reference: 'Elastic collisions/',
+          tag: $scope.tags["phys"]
+        },{
+          name: 'Feigenbaum',
+          description: 'Visualization of how chaos emerges from applying simple functions repeatedly.',
+          reference: 'Feigenbaum/',
+          tag: $scope.tags["math"]
+        },{
+          name: 'Interactive Fractals',
+          description: 'Some famous fractals and sliders and knobs for controlling them.',
+          reference: 'Interactive Fractals/',
+          tag: $scope.tags["math"]
+        },{
+          name: 'Ising model',
+          description: "A real-time visualization of the Ising model for a two-dimensional grid of half-integer spin particles. You can control inverse temperature to discover the ferromagnetic phase transition.",
+          reference: 'Ising model/',
+          tag: $scope.tags["phys"]
+        },{
+          name: 'Lennard Jones force networks',
+          description: 'Just a silly simulation of particles interacting via the Lennard Jones potential.',
+          reference: 'Lennard Jones force networks/',
+          tag: $scope.tags["phys"]
+        },{
+          name: 'The Wave Equation',
+          description: 'Real-time simulation of the wave equation with custom barriers and damping.',
+          reference: 'The Wave Equation/',
+          tag: $scope.tags["phys"]
+        },{
+          name: 'Planetary',
+          description: 'Simulation of planets (or circles if you want) that interact gravitationally and merge conserving mass (area), momentum and center of mass.',
+          reference: 'Planetary/',
+          tag: $scope.tags["phys"]
+        },{
+          name: 'Rainbow',
+          description: 'A devoted rendering of how rays refracts and reflects through a drop of water.',
+          reference: 'Rainbow/',
+          tag: $scope.tags["phys"]
+        }
+      ];
+});
