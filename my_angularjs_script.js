ae6d67bdc00043a2068709f1a979dc16c7551c45
August Geelmuyden <augustgeelmuyden92@gmail.com>
2018-09-21 19:57:52 +0200

    Quaternions

--- my_angularjs_script.js
+++ my_angularjs_script.js
@@ -77,12 +77,12 @@ var mainCtrl = app.controller("mainCtrl", function ($scope) {
       reference: "FluidMechanics",
       tag: $scope.tags["phys"],
       language: $scope.languages["english"]},
-    // {
-    //   name: 'QFT in Curved Spacetime',
-    //   description: "...",
-    //   reference: "QFT in Curved Spacetime",
-    //   tag: $scope.tags["phys"],
-    //   language: $scope.languages["english"]},
+    {
+      name: 'Quaternioic Babbling',
+      description: "A text exploring the history and algebra of the Quaternions.",
+      reference: "Quaternions",
+      tag: $scope.tags["math"],
+      language: $scope.languages["english"]},
     {
       name: 'Test post',
       description: "This is just a post to test functionality of the web page.",
