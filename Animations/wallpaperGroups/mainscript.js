5bbd297bef0105b5a6796a7ec3bdb49d7a708c7e
August Geelmuyden <augustgeelmuyden92@gmail.com>
2019-02-17 11:38:49 +0000

    added all 17 wallpaper groups

--- Animations/wallpaperGroups/mainscript.js
+++ Animations/wallpaperGroups/mainscript.js
@@ -1,7 +1,7 @@
 
 
 var S = [[150,0],[0,150]]; // spanning lattice square
-
+var Hex;
 var bufferSize = [150,150];
 
 var SYM;
@@ -12,26 +12,40 @@ function setup(){
   createCanvas(window.innerWidth-10, window.innerHeight-10);
 
   // blendMode(MULTIPLY);
+  Hex = [[0,150],[150*sin(PI/3),150*cos(PI/3)]];
 
   SYM = new SymmetryBase(S);
   var symmetry = generate_p2(S[0],S[1])
   SYM.symmetries = SYM.symmetries.concat(symmetry)
 
-  // var symmetry = generate_p2(S[0],S[1])
-  // var symmetry = generate_pm(S[0],S[1])
-  // var symmetry = generate_pg(S[0],S[1])
-  // var symmetry = generate_cm(S[0],S[1])
-  // var symmetry = generate_pmm(S[0],S[1])
-
+  dropdownC = createSelect();
+  dropdownC.option("Black ",  color(47, 52, 59,   50) )
+  dropdownC.option("White ",  color(255, 255,255, 50) )
+  dropdownC.option("Red "  ,  color(212, 50, 21,  50) )
+  dropdownC.option("Orange ", color(255, 149, 11, 50) )
+  dropdownC.option("Blue ",   color(86, 190, 215, 50) )
+  dropdownC.option("Green ",  color(70, 139,   0, 50) )
+  dropdownC.changed(dropdownColorChanged)
+  dropdownC.position(50,30)
 
   dropdown = createSelect();
-  dropdown.option("P1 ", 0 )
-  dropdown.option("P2 ", 1 )
-  dropdown.option("Pm ", 2 )
-  dropdown.option("Pg ", 3 )
-  dropdown.option("Cm ", 4 )
-  dropdown.option("Pmm", 5 )
-  dropdown.option("Pmg", 6 )
+  dropdown.option("P1 ",   0 )
+  dropdown.option("P2 ",   1 )
+  dropdown.option("Pm ",   2 )
+  dropdown.option("Pg ",   3 )
+  dropdown.option("Cm ",   4 )
+  dropdown.option("Pmm",   5 )
+  dropdown.option("Pmg",   6 )
+  dropdown.option("Pgg",   7 )
+  dropdown.option("Cmm",   8 )
+  dropdown.option("P4",    9 )
+  dropdown.option("P4m",  10 )
+  dropdown.option("P4g",  11 )
+  dropdown.option("P3",   12 )
+  dropdown.option("P3m1", 13 )
+  dropdown.option("P31m", 14 )
+  dropdown.option("P6",   15 )
+  dropdown.option("P6m",  16 )
   dropdown.changed(dropdownChanged)
   dropdown.selected(1)
   dropdown.position(50,50)
@@ -39,6 +53,8 @@ function setup(){
   button = createButton('clear canvas');
   button.position(50,70);
   button.mousePressed(clearCanvas);
+
+  strokeWeight(2);
 
   background(255);
   translate(width/2,height/2)
@@ -61,8 +77,9 @@ function draw(){
 
 function SymmetryBase(S){
   this.S = S;
+  this.color = color(0,0,0,50)
   this.points = [];
-  this.symmetries = [new Translation(S[0],S[1],1,1)];
+  this.symmetries = [new Translation(S[0],S[1],2,2)];
   // this.symmetries = [];
 
   this.showLattice = function(){
@@ -130,7 +147,7 @@ function SymmetryBase(S){
   }
 
   this.showPoint = function(x,y){
-    stroke(0)
+    stroke(this.color)
     point(x,y,3,3)
   }
 
@@ -356,9 +373,6 @@ function generate_pmm(S1,S2){
 
 function generate_pmg(S1,S2){
   var P = []
-  // var cx = (S1[0]+S2[0]/2)/2
-  // var cy = (S1[1]+S2[1]/2)/2
-  // P.push( new Rotation(cx,cy,2) )
   var cx = (S1[0]/2+S2[0])/2
   var cy = (S1[1]/2+S2[1])/2
   P.push( new Rotation(cx,cy,2) )
@@ -369,6 +383,121 @@ function generate_pmg(S1,S2){
   var c = [S2[0]/2,S2[1]/2]
   var d = [S1[0],S1[1]]
   P.push( new GlideReflection(c,d,L) )
+  return P
+}
+
+function generate_pgg(S1,S2){
+  var P = []
+  var cx = (S1[0]+S2[0])/2
+  var cy = (S1[1]+S2[1])/2
+  P.push( new Rotation(cx,cy,2) )
+  var L = sqrt(sq(S1[0])+sq(S1[1]))/2
+  var c = [S2[0]/4,S2[1]/4]
+  var d = [S1[0],S1[1]]
+  P.push( new GlideReflection(c,d,L) )
+  var L = sqrt(sq(S2[0])+sq(S2[1]))/2
+  var c = [S1[0]/4,S1[1]/4]
+  var d = [S2[0],S2[1]]
+  P.push( new GlideReflection(c,d,L) )
+  return P
+}
+
+function generate_cmm(S1,S2){
+  var P = []
+  var cx = (S1[0]+S2[0])/2
+  var cy = (S1[1]+S2[1])/2
+  P.push( new Rotation(cx,cy,2) )
+  var c = [S1[0]/2,S1[0]/2]
+  var d = [S2[0],S2[1]]
+  P.push( new Mirror(c,d) )
+  var L = sqrt(sq(S1[0])+sq(S1[1]))/2
+  var c = [S2[0]/4,S2[1]/4]
+  var d = [S1[0],S1[1]]
+  P.push( new GlideReflection(c,d,L) )
+  return P
+}
+
+function generate_p4(S1,S2){
+  var P = []
+  var cx = (S1[0]+S2[0])/2
+  var cy = (S1[1]+S2[1])/2
+  P.push( new Rotation(cx,cy,4) )
+  return P
+}
+
+function generate_p4m(S1,S2){
+  var P = []
+  var cx = (S1[0]+S2[0])/2
+  var cy = (S1[1]+S2[1])/2
+  P.push( new Rotation(cx,cy,4) )
+  var c = [S1[0]/2,S1[1]/2]
+  var d = [S2[0],S2[1]]
+  P.push( new Mirror(c,d) )
+  return P
+}
+
+function generate_p4g(S1,S2){
+  var P = []
+  var cx = (S1[0]+S2[0])/2
+  var cy = (S1[1]+S2[1])/2
+  P.push( new Rotation(cx,cy,4) )
+  var L = sqrt(sq(S1[0])+sq(S1[1]))/2
+  var c = [S2[0]/2,S2[1]/2]
+  var d = [S1[0],S1[1]]
+  P.push( new GlideReflection(c,d,L) )
+  var c = [S1[0]/4,S1[1]/4]
+  var d = [S2[0],S2[1]]
+  P.push( new Mirror(c,d) )
+  return P
+}
+
+
+function generate_p3(S1,S2){
+  var P = []
+  var cx = (S1[0]+S2[0])/3
+  var cy = (S1[1]+S2[1])/3
+  P.push( new Rotation(cx,cy,3) )
+  return P
+}
+
+function generate_p3m1(S1,S2){
+  var P = []
+  var cx = (S1[0]+S2[0])/3
+  var cy = (S1[1]+S2[1])/3
+  P.push( new Rotation(cx,cy,3) )
+  var c = [0,0]
+  var d = [S1[0]+S2[0],S1[1]+S2[1]]
+  P.push( new Mirror(c,d) )
+  return P
+}
+
+function generate_p31m(S1,S2){
+  var P = []
+  var cx = (S1[0]+S2[0])/3
+  var cy = (S1[1]+S2[1])/3
+  P.push( new Rotation(cx,cy,3) )
+  var c = [S1[0],S1[1]]
+  var d = [S2[0]-S1[0],S2[1]-S1[1]]
+  P.push( new Mirror(c,d) )
+  return P
+}
+
+function generate_p6(S1,S2){
+  var P = []
+  var cx = 0
+  var cy = 0
+  P.push( new Rotation(cx,cy,6) )
+  return P
+}
+
+function generate_p6m(S1,S2){
+  var P = []
+  var cx = 0
+  var cy = 0
+  P.push( new Rotation(cx,cy,6) )
+  var c = [S1[0],S1[1]]
+  var d = [S2[0]-S1[0],S2[1]-S1[1]]
+  P.push( new Mirror(c,d) )
   return P
 }
 
@@ -388,25 +517,65 @@ function generate_pmg(S1,S2){
 
 function dropdownChanged(){
   SYM = new SymmetryBase(S);
-  var translations = [new Translation(S[0],S[1],1,1)];
+  var translations = [new Translation(S[0],S[1],2,2)];
 
   if(this.value()==0){
+    var gridVecs = S;
     var symmetry = generate_p1(S[0],S[1])
   }else if(this.value()==1){
+    var gridVecs = S;
     var symmetry = generate_p2(S[0],S[1])
   }else if(this.value()==2){
+    var gridVecs = S;
     var symmetry = generate_pm(S[0],S[1])
   }else if(this.value()==3){
+    var gridVecs = S;
     var symmetry = generate_pg(S[0],S[1])
   }else if(this.value()==4){
+    var gridVecs = S;
     var symmetry = generate_cm(S[0],S[1])
   }else if(this.value()==5){
+    var gridVecs = S;
     var symmetry = generate_pmm(S[0],S[1])
   }else if(this.value()==6){
+    var gridVecs = S;
     var symmetry = generate_pmg(S[0],S[1])
+  }else if(this.value()==7){
+    var gridVecs = S;
+    var symmetry = generate_pgg(S[0],S[1])
+  }else if(this.value()==8){
+    var gridVecs = S;
+    var symmetry = generate_cmm(S[0],S[1])
+  }else if(this.value()==9){
+    var gridVecs = S;
+    var symmetry = generate_p4(S[0],S[1])
+  }else if(this.value()==10){
+    var gridVecs = S;
+    var symmetry = generate_p4m(S[0],S[1])
+  }else if(this.value()==11){
+    var gridVecs = S;
+    var symmetry = generate_p4g(S[0],S[1])
+  }else if(this.value()==12){
+    var gridVecs = Hex;
+    var symmetry = generate_p3(Hex[0],Hex[1])
+  }else if(this.value()==13){
+    var gridVecs = Hex;
+    var symmetry = generate_p3m1(Hex[0],Hex[1])
+  }else if(this.value()==14){
+    var gridVecs = Hex;
+    var symmetry = generate_p31m(Hex[0],Hex[1])
+  }else if(this.value()==15){
+    var gridVecs = Hex;
+    var symmetry = generate_p6(Hex[0],Hex[1])
+  }else if(this.value()==16){
+    var gridVecs = Hex;
+    var symmetry = generate_p6m(Hex[0],Hex[1])
   }else{
     var symmetry = []
   }
+
+  SYM = new SymmetryBase(gridVecs);
+  var translations = [new Translation(gridVecs[0],gridVecs[1],2,2)];
 
   SYM.symmetries = SYM.symmetries.concat(symmetry)
   background(255)
@@ -417,6 +586,11 @@ function dropdownChanged(){
   // var symmetry = generate_pg(S[0],S[1])
   // var symmetry = generate_cm(S[0],S[1])
   // var symmetry = generate_pmm(S[0],S[1])
+}
+
+
+function dropdownColorChanged(){
+  SYM.color = this.value();
 }
 
 
