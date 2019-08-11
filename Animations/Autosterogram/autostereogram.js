6ca3d2bdc742db29e2b233714623e2e9c8c9f816
August Geelmuyden <augustgeelmuyden92@gmail.com>
2019-08-11 23:28:12 +0100

    Added draw mode

--- Animations/Autosterogram/autostereogram.js
+++ Animations/Autosterogram/autostereogram.js
@@ -3,13 +3,15 @@
 var t = 0;
 var s = 5;
 var asg,sel,W,H;
+var showDrawing = false;
+var cursorDepth = -2
 
 function setup(){
   C = createCanvas(windowWidth, windowHeight);
   C.parent("sketch-holder");
 
-  W = width/s;
-  H = height/s;
+  W = int(width/s);
+  H = int(height/s);
 
   asg = new Autostereogram(W,H)
   asg.initiateMatrix(40)
@@ -22,6 +24,7 @@ function setup(){
   sel.option('Sharp shapes');
   sel.option('Smooth shapes');
   sel.option('Dynamic shapes');
+  sel.option('Draw Image');
   // sel.changed(mySelectEvent);
 }
 
@@ -31,6 +34,9 @@ function draw(){
   background(255);
   // scale(s);
   asg.show(0,0);
+  if(sel.value()=='Draw Image' && showDrawing){
+    asg.showMatrix()
+  }
   // asg.showAt(50,0)
   t += 1;
 }
@@ -43,11 +49,18 @@ function Z(x,y){
   }else if(sel.value()=='Smooth shapes'){
     z -= 3*(1+cos((2*PI/80)*sqrt(sq(x-W/2)+sq(y-H/2))))*exp(-0.0001*(sq(x-W/2)+sq(y-H/2)));
     // z -= 3*(1+cos((2*PI/80)*sqrt(sq(x-W/2-100)+sq(y-H/2))))*exp(-0.0001*(sq(x-W/2-100)+sq(y-H/2)));
+  }else if(sel.value()=='Dynamic shapes'){
+    z -= int(3*(1+cos((2*PI/50)*sqrt(sq(x-W/2)+sq(y-H/2))-t))*exp(-0.0001*(sq(x-W/2)+sq(y-H/2))));
   }else{
-    z -= 3*(1+cos((2*PI/50)*sqrt(sq(x-W/2)+sq(y-H/2))-t))*exp(-0.0001*(sq(x-W/2)+sq(y-H/2)));
+    var i = min(asg.W-1,max(0,x))
+    var j = min(asg.h-1,max(0,y))
+    z -= asg.matrix[i][j]
+
   }
   return z
 }
+
+
 
 function Autostereogram(W,H){
   this.W = W
@@ -71,6 +84,27 @@ function Autostereogram(W,H){
       }
       this.M.push(subM)
       this.Zs.push(subZ)
+    }
+
+    this.matrix = [];
+    for(var i=0; i<this.W; i++){
+      var subMat = []
+      for(var j=0; j<this.h; j++){
+        subMat.push(0)
+      }
+      this.matrix.push(subMat)
+    }
+  }
+
+  this.setMatrix = function(x,y,z){
+    var i0 = int(x/s);
+    var j0 = int(y/s);
+    for(var di=-3; di<3; di++){
+      for(var dj=-3; dj<3; dj++){
+        var i = min(max(i0+di,0),asg.W);
+        var j = min(max(j0+dj,0),asg.h);
+        this.matrix[i][j] = z;
+      }
     }
   }
 
@@ -103,14 +137,35 @@ function Autostereogram(W,H){
     }
   }
 
+  this.showMatrix = function(){
+    for(var i=0; i<this.W; i++){
+      for(var j=0; j<this.h; j++){
+        if(this.matrix[i][j]!=0){
+          fill(-255*(this.matrix[i][j]/5))
+          rect(s*i,s*j,s,s)
+        }
+      }
+    }
+  }
+
 }
 
+function mouseDragged(){
+  asg.setMatrix(mouseX,mouseY,cursorDepth)
+}
 
-// function mySelectEvent(){
-//   // if(sel.value()=='Dynamic shapes'){
-//   //   loop();
-//   // }else{
-//   //   noLoop();
-//   // }
-//   // redraw();
-// }
+function mousePressed(){
+  asg.setMatrix(mouseX,mouseY,cursorDepth)
+}
+
+function keyPressed(){
+  if(keyCode==32){ // SPACE
+    showDrawing = !showDrawing;
+  }else if(keyCode==38){ // UP
+    cursorDepth += 1;
+    cursorDepth = min(max(cursorDepth,-5),0)
+  }else if(keyCode==40){
+    cursorDepth -= 1;
+    cursorDepth = min(max(cursorDepth,-5),0)
+  }
+}
